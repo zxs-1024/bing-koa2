9374f5a0395ae9406b791390f0c9f16546ddde1a
hao.zhang <735296233@qq.com>
2019-01-21 17:17:17 +0800

    :sparkles: 添加年月查询

--- controller/ImageController.js
+++ controller/ImageController.js
@@ -8,15 +8,18 @@ const Detail = mongoose.model('Detail')
 const { multiTableQuery } = require('../utils/multiTableQuery')
 const schedule = require('../utils/schedule.js')
 
+// 默认倒序
 const sort = { date: -1 }
 
-// 添加定时任务
+// 定时任务
 schedule()
 
 class ImageController {
   static async getImagesAll(ctx) {
     const count = await Image.count({})
+
     console.log(`🔥  检索到 ${count} 条列表数据 !`)
+
     if (count) {
       const collect = await Image.find({})
         .sort(sort)
@@ -31,23 +34,50 @@ class ImageController {
   static async getImagesById(ctx) {
     const { id } = ctx.params
     const image = await Image.findById(id).populate({ path: 'detail' })
+
     ctx.body = image
   }
 
+  // 查询单日
   static async getImageByDate(ctx) {
     const { date } = ctx.params
     const images = await Image.find({
       dateString: { $regex: date }
     }).sort(sort)
+
     ctx.body = images
   }
 
+  // 查询单月
+  static async getImageByMonth(ctx) {
+    const { month } = ctx.params
+    const images = await Image.find({
+      dateString: { $regex: month }
+    }).sort({ date: 1 })
+
+    ctx.body = images
+  }
+
+  // 查询单年
+  static async getImageByYear(ctx) {
+    const { year } = ctx.params
+    const re = new RegExp(`${year}[0-9]{2}(.*)01`)
+
+    const images = await Image.find({
+      dateString: { $regex: re }
+    }).sort({ date: 1 })
+
+    ctx.body = images
+  }
+
+  // 分页查询 默认 10
   static async getImageByPage(ctx) {
     const { page, limit = 10 } = ctx.params
     const images = await Image.paginate(
       {},
       { page, limit: Number(limit), sort }
     )
+
     ctx.body = images
   }
 }
--- routes/bing.js
+++ routes/bing.js
@@ -6,6 +6,8 @@ router.prefix('/bing/api/v1')
 router.get('/images', ImageController.getImagesAll)
 router.get('/images/:id', ImageController.getImagesById)
 router.get('/images/date/:date', ImageController.getImageByDate)
+router.get('/images/month/:month', ImageController.getImageByMonth)
+router.get('/images/year/:year', ImageController.getImageByYear)
 router.get('/images/:page/:limit', ImageController.getImageByPage)
 
 module.exports = router
